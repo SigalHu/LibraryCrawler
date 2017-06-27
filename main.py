6ac265f472bac1bdb77799fde6adac14b07dae7c
huxujun-PC\huxujun <811734597@qq.com>
2017-06-27 18:16:09 +0800

    简化操作

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import urllib.request
+import urllib.parse
+import http.cookiejar
 import re
 import os
 import time
@@ -9,7 +11,7 @@ from bs4 import BeautifulSoup
 
 
 class LibraryCrawler:
-	__url_pic = None
+	__book_page_url = None
 	__book_name = None
 	__book_items = {'封面页': ['cov%03d.jpg'],
 	                '书名页': ['bok%03d.jpg'],
@@ -47,50 +49,105 @@ class LibraryCrawler:
 			self.__book_items[key].append(start_page)
 			self.__book_items[key].append(end_page)
 
-	def __init_para(self, url):
-		resp = urllib.request.urlopen(url)
-		page = resp.read().decode('utf-8')
+	def __init_para(self, book_url):
+		resp = urllib.request.urlopen(book_url)
+		html_page = resp.read().decode('utf-8')
 
-		results = re.findall(re.compile('<title>(.*?)</title>'), page)
+		results = re.findall(re.compile('<title>(.*?)</title>'), html_page)
 		if len(results):
 			self.__book_name = re.sub(re.compile(r'[\\/:*?"<>|]+'), ' ', results[0]).strip(' ')
 		else:
 			raise Exception('获取书籍地址失败！')
 
-		results = re.findall(re.compile(r'var str=\'(.*?)\''), page)
+		results = re.findall(re.compile(r'var str=\'(.*?)\''), html_page)
 		if len(results):
-			self.__url_pic = results[0]
+			self.__book_page_url = results[0]
 		else:
 			raise Exception('获取书籍地址失败！')
 
 		results = re.findall(re.compile(
 			r'pages :\[\[(.*?),(.*?)\],\[(.*?),(.*?)\],\[(.*?),(.*?)\],\[(.*?),(.*?)\], \[(.*?),(.*?)\], \[spage, epage\]'),
-			page)
+			html_page)
 		if len(results) and len(results[0]) == (len(self.__book_items) - 1) * 2:
 			for ii, key in zip(range(len(self.__book_items) - 1), self.__book_items.keys()):
 				self.__set_book_items(key, int(results[0][2 * ii]), int(results[0][2 * ii + 1]))
 		else:
 			raise Exception('获取书籍栏目失败！')
 
-		results = re.findall(re.compile(r'var spage = (.*?), epage = (.*?);'), page)
+		results = re.findall(re.compile(r'var spage = (.*?), epage = (.*?);'), html_page)
 		if len(results) and len(results[0]) == 2:
 			self.__set_book_items('正文页', int(results[0][0]), int(results[0][1]))
 		else:
 			raise Exception('获取书籍栏目失败！')
 
-	def __get_book_url(self,html_page):
+	def __get_book_url(self, book_info_url):
+		ssid_para = {'callback': 'SigalHu',
+		             'isbn': '',
+		             'bookName': '',
+		             'author': '',
+		             'eCode': 'utf-8'}
+		# 获取书籍信息页面
+		resp = urllib.request.urlopen(book_info_url)
+		html_page = resp.read().decode('utf-8')
+
+		# 获取请求参数
 		soup = BeautifulSoup(html_page, 'html.parser')
-		results = soup.select('.booklist')
-		for res in results:
-			book_info = re.findall(re.compile(r'<dt>(.*?):</dt>'),res)
-			if len(book_info) == 1:
-				book_info = book_info[0]
-			else:
-				pass
+		res = soup.find('dt', text=re.compile(r'ISBN及定价'))
+		if res is None:
+			res = soup.find('dt', text=re.compile(r'标准书号'))
+		if res is not None:
+			isbn = res.find_next_sibling('dd').string.strip()
+			if isbn is not None:
+				ssid_para['isbn'] = isbn.split(' ')[0].split("/")[0]
 
+		res = soup.find('dt', text=re.compile(r'题名/责任者'))
+		if res is not None:
+			book_name = ''
+			for child_tag in res.find_next_sibling('dd').children:
+				book_name += child_tag.string
+			book_name = book_name.strip().split('/')
+			ssid_para['bookName'] = book_name[0]
+			if len(book_name) > 1:
+				ssid_para['author'] = book_name[1]
 
-	def download_jpg(self, url, save_path):
-		self.__init_para(url)
+		res = soup.find('dt', text=re.compile(r'个人责任者'))
+		if res is not None:
+			author = ''
+			for child_tag in res.find_next_sibling('dd').children:
+				author += child_tag.string
+			author = author.strip().split(' ')
+			if len(author) > 0:
+				ssid_para['author'] = author[0]
+
+		# 获取ssid
+		resp = urllib.request.urlopen(r'http://202.119.70.51:8088/servlet/isExitJson?' + urllib.parse.urlencode(ssid_para))
+		ssid_data = re.findall(re.compile(r'"ssid":"(\d+)"'), resp.read().decode('utf-8'))
+		if len(ssid_data) == 0:
+			raise Exception('该书籍不存在电子版！')
+
+		# 获取book_cookie
+		cookies = http.cookiejar.CookieJar()
+		handler = urllib.request.HTTPCookieProcessor(cookies)
+		opener = urllib.request.build_opener(handler)
+		resp = opener.open(r'http://202.119.70.51:8088/catchpage/URL.jsp?BID=' + ssid_data[0])
+		book_cookie = ''
+		for cookie in cookies:
+			book_cookie += '%s=%s;' % (cookie.name, cookie.value)
+
+		# 获取book_url
+		resq = urllib.request.Request(r'http://202.119.70.51:8088/markbook/guajie.jsp?BID=' + ssid_data[0],
+		                              headers={'Cookie': book_cookie})
+		resp = opener.open(resq)
+		resq = urllib.request.Request(r'http://202.119.70.51:8088/getbookread?BID=' + ssid_data[
+			0] + '&ReadMode=0&jpgread=0&displaystyle=0&NetUser=&page=',
+		                              headers={'Cookie': book_cookie})
+		resp = opener.open(resq)
+		book_url = r'http://202.119.70.51:8088' + urllib.parse.unquote(resp.read().decode('utf-8'))
+		return book_url
+
+	def download_jpg(self, book_info_url, save_path):
+		book_url = self.__get_book_url(book_info_url)
+		self.__init_para(book_url)
 
 		if os.path.exists(save_path):
 			if not os.path.isdir(save_path):
@@ -112,12 +169,13 @@ class LibraryCrawler:
 				os.mkdir(path)
 				for ii in tqdm(range(value[1], value[2] + 1)):
 					pic_name = value[0] % ii
-					urllib.request.urlretrieve(self.__url_pic + pic_name, os.path.join(path, pic_name))
+					urllib.request.urlretrieve(self.__book_page_url + pic_name, os.path.join(path, pic_name))
 				time.sleep(0.1)
 		print('《%s》下载完毕！' % self.__book_name)
 
-	def download_pdf(self, url, save_path):
-		self.__init_para(url)
+	def download_pdf(self, book_info_url, save_path):
+		book_url = self.__get_book_url(book_info_url)
+		self.__init_para(book_url)
 
 		if os.path.exists(save_path):
 			if not os.path.isdir(save_path):
@@ -128,7 +186,7 @@ class LibraryCrawler:
 		if os.path.exists(pdf_path):
 			print('文件：%s 已存在，停止下载《%s》！' % (pdf_path, self.__book_name))
 			return
-		resp = urllib.request.urlopen(self.__url_pic + self.__book_items['正文页'][0] % self.__book_items['正文页'][1])
+		resp = urllib.request.urlopen(self.__book_page_url + self.__book_items['正文页'][0] % self.__book_items['正文页'][1])
 		img = Image.open(resp)
 		canvas = Canvas(pdf_path, pagesize=img.size)
 
@@ -139,7 +197,7 @@ class LibraryCrawler:
 				time.sleep(0.1)
 				for ii in tqdm(range(value[1], value[2] + 1)):
 					pic_name = value[0] % ii
-					canvas.drawImage(self.__url_pic + pic_name, 0, 0, img.size[0], img.size[1])
+					canvas.drawImage(self.__book_page_url + pic_name, 0, 0, img.size[0], img.size[1])
 					canvas.showPage()
 				time.sleep(0.1)
 		canvas.save()
@@ -182,9 +240,6 @@ class LibraryCrawler:
 
 if __name__ == '__main__':
 	save_path = 'files'
-	url = 'http://202.119.70.51:8088/Jpath_sky/DsrPath.do?code=B9F43CAD3FD709CD577FA3A6F1D46ECE&ssnumber=13176387&netuser=1&jpgreadmulu=1&displaystyle=0&channel=0&ipside=0'
+	url = r'http://202.119.70.22:888/opac/item.php?marc_no=0000739835'
 	lc = LibraryCrawler()
-	# lc.download_pdf(url,save_path)
-	lc.jpg_to_pdf(r'''files/HTML5与CSS3网页设计入门与提高''', save_path)
-	print(lc.book_name)
-	print(lc.book_items)
+	lc.download_pdf(url,save_path)
