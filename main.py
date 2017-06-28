aea1d9d981c9b6ceb7303593fe5711c7795a9fd5
huxujun-PC\huxujun <811734597@qq.com>
2017-06-29 00:08:48 +0800

    新增下载电子资源相关代码

--- main.py
+++ main.py
@@ -78,15 +78,12 @@ class LibraryCrawler:
 		else:
 			raise Exception('获取书籍栏目失败！')
 
-	def __get_book_url(self, book_info_url):
+	def __get_book_url(self, html_page):
 		ssid_para = {'callback': 'SigalHu',
 		             'isbn': '',
 		             'bookName': '',
 		             'author': '',
 		             'eCode': 'utf-8'}
-		# 获取书籍信息页面
-		resp = urllib.request.urlopen(book_info_url)
-		html_page = resp.read().decode('utf-8')
 
 		# 获取请求参数
 		soup = BeautifulSoup(html_page, 'html.parser')
@@ -105,6 +102,7 @@ class LibraryCrawler:
 				book_name += child_tag.string
 			book_name = book_name.strip().split('/')
 			ssid_para['bookName'] = book_name[0]
+			self.__book_name = re.sub(re.compile(r'[\\/:*?"<>|]+'), ' ', book_name[0]).strip(' ')
 			if len(book_name) > 1:
 				ssid_para['author'] = book_name[1]
 
@@ -146,6 +144,49 @@ class LibraryCrawler:
 		book_url = r'http://202.119.70.51:8088' + urllib.parse.unquote(resp.read().decode('utf-8'))
 		return book_url
 
+	def __get_iso_url(self, html_page):
+		js_para = {'srchtype': 'I',
+		           'wd': None,
+		           'clienttype': 'L',
+		           'ISBN': None}
+		js_url = r'http://202.119.70.28/emlib4/system/datasource/opacinterface.aspx?'
+		iso_url_list = []
+
+		# 获取请求参数
+		soup = BeautifulSoup(html_page, 'html.parser')
+		res = soup.find('dt', text=re.compile(r'^ISBN'))
+		if res is None:
+			raise Exception('获取ISO相关参数失败！')
+		isbn = res.find_next_sibling('dd').string.strip()
+		if isbn is None:
+			raise Exception('获取ISO相关参数失败！')
+		isbn = re.findall(re.compile(r'((\d+-)+\d+)'), isbn)
+		if len(isbn) == 0 or len(isbn[0]) < 2:
+			raise Exception('获取ISO相关参数失败！')
+		js_para['wd'] = isbn[0][0]
+		js_para['ISBN'] = isbn[0][0]
+
+		resp = urllib.request.urlopen(js_url + urllib.parse.urlencode(js_para))
+		js_page = resp.read().decode('utf-8')
+		results = re.findall(re.compile(r'javascript:SubmitURL\("post","(.*?)"\+escape\("(.*?)"\)\)'), js_page)
+		if len(results) and len(results[0]) > 1:
+			iso_url_list = results[0][0] + urllib.parse.quote(results[0][1])
+		else:
+			results = re.findall(re.compile(
+				r'(http://202\.119\.70\.28/emlib4/system/datasource/opendataobjectdetails\.aspx\?doRUID=[0-9A-Za-z]+)\\'),
+				js_page)
+			if len(results) == 0:
+				raise Exception('获取ISO相关参数失败')
+			resp = urllib.request.urlopen(results[0])
+			resource_page = resp.read().decode('utf-8')
+			results = re.findall(re.compile(r'javascript:SubmitURL\("post","(.*?)"\+escape\("(.*?)"\)\)'),
+			                     resource_page)
+			if len(results):
+				for res in results:
+					if len(res) > 1:
+						iso_url_list.append(res[0] + urllib.parse.quote(res[1]))
+		return iso_url_list
+
 	def __get_book_list(self, html_page):
 		soup = BeautifulSoup(html_page, 'html.parser')
 		book_info_list = soup.find_all('li', class_='book_list_info')
@@ -168,130 +209,222 @@ class LibraryCrawler:
 				book_list.append(book_info)
 		return book_list
 
+	def __download_iso(self, iso_url, save_path, iso_name):
+		try:
+			if not iso_name.lower().endswith('.iso'):
+				iso_name += '.iso'
+			iso_path = os.path.join(save_path, iso_name)
+			if os.path.exists(iso_path):
+				raise Exception('%s 已存在！' % iso_name)
+			print('开始下载 %s...' % iso_name)
+			time.sleep(0.1)
+
+			with tqdm(unit='B', unit_scale=True, leave=True, miniters=1) as t:
+				def report_progress(download_num=1, download_size=1, total_size=None):
+					if total_size is not None:
+						t.total = total_size
+					t.update(download_size)
+
+				urllib.request.urlretrieve(iso_url, filename=iso_path,
+				                           reporthook=report_progress, data=None)
+			time.sleep(0.1)
+			print('%s 下载完毕！' % iso_name)
+			return True
+		except Exception as ex:
+			print(ex)
+			return False
+
 	def search_books(self, key_word):
-		search_para = {'strSearchType': 'title',
-		               'match_flag': 'forward',
-		               'historyCount': '0',
-		               'strText': key_word,
-		               'doctype': 'ALL',
-		               'with_ebook': 'on',
-		               'displaypg': '10000',
-		               'showmode': 'list',
-		               'sort': 'CATA_DATE',
-		               'orderby': 'desc',
-		               'dept': 'ALL',
-		               'page': '1'}
-		resp = urllib.request.urlopen(
-			r'http://202.119.70.22:888/opac/openlink.php?' + urllib.parse.urlencode(search_para))
-		html_page = resp.read().decode('utf-8')
-		book_list = self.__get_book_list(html_page)
-
-		page_list = re.findall(re.compile(r'<option value=\'\d+\'>(\d+)</option>'), html_page)
-		if len(page_list):
-			page_list = page_list[:int(len(page_list) / 2)]
-
-		for page_num in page_list:
-			search_para['page'] = page_num
+		try:
+			search_para = {'strSearchType': 'title',
+			               'match_flag': 'forward',
+			               'historyCount': '0',
+			               'strText': key_word,
+			               'doctype': 'ALL',
+			               'with_ebook': 'on',
+			               'displaypg': '10000',
+			               'showmode': 'list',
+			               'sort': 'CATA_DATE',
+			               'orderby': 'desc',
+			               'dept': 'ALL',
+			               'page': '1'}
 			resp = urllib.request.urlopen(
 				r'http://202.119.70.22:888/opac/openlink.php?' + urllib.parse.urlencode(search_para))
 			html_page = resp.read().decode('utf-8')
-			book_list += self.__get_book_list(html_page)
-		return book_list
+			book_list = self.__get_book_list(html_page)
 
-	def download_jpg(self, book_info_url, save_path):
-		book_url = self.__get_book_url(book_info_url)
-		self.__init_para(book_url)
+			page_list = re.findall(re.compile(r'<option value=\'\d+\'>(\d+)</option>'), html_page)
+			if len(page_list):
+				page_list = page_list[:int(len(page_list) / 2)]
 
-		if os.path.exists(save_path):
-			if not os.path.isdir(save_path):
-				raise Exception('保存路径错误！')
-		else:
-			os.mkdir(save_path)
-		root_path = os.path.join(save_path, self.__book_name)
-		if os.path.exists(root_path):
-			print('文件夹：%s 已存在，停止下载《%s》！' % (root_path, self.__book_name))
-			return
-		os.mkdir(root_path)
+			for page_num in page_list:
+				search_para['page'] = page_num
+				resp = urllib.request.urlopen(
+					r'http://202.119.70.22:888/opac/openlink.php?' + urllib.parse.urlencode(search_para))
+				html_page = resp.read().decode('utf-8')
+				book_list += self.__get_book_list(html_page)
+			return book_list
+		except Exception as ex:
+			print(ex)
+			return []
 
-		print('开始下载《%s》...' % self.__book_name)
-		for key, value in zip(self.__book_items.keys(), self.__book_items.values()):
-			if value[1] <= value[2]:
-				print('正在下载%s...' % key)
-				time.sleep(0.1)
-				path = os.path.join(root_path, key)
-				os.mkdir(path)
-				for ii in tqdm(range(value[1], value[2] + 1)):
-					pic_name = value[0] % ii
-					urllib.request.urlretrieve(self.__book_page_url + pic_name, os.path.join(path, pic_name))
-				time.sleep(0.1)
-		print('《%s》下载完毕！' % self.__book_name)
+	def download_jpg(self, book_info_url, save_path, is_download_iso=False):
+		html_page = ''
+		try:
+			# 获取书籍信息页面
+			resp = urllib.request.urlopen(book_info_url)
+			html_page = resp.read().decode('utf-8')
 
-	def download_pdf(self, book_info_url, save_path):
-		book_url = self.__get_book_url(book_info_url)
-		self.__init_para(book_url)
+			book_url = self.__get_book_url(html_page)
+			self.__init_para(book_url)
 
-		if os.path.exists(save_path):
-			if not os.path.isdir(save_path):
-				raise Exception('保存路径错误！')
-		else:
-			os.mkdir(save_path)
-		pdf_path = os.path.join(save_path, self.__book_name + '.pdf')
-		if os.path.exists(pdf_path):
-			print('文件：%s 已存在，停止下载《%s》！' % (pdf_path, self.__book_name))
-			return
-		resp = urllib.request.urlopen(self.__book_page_url + self.__book_items['正文页'][0] % self.__book_items['正文页'][1])
-		img = Image.open(resp)
-		canvas = Canvas(pdf_path, pagesize=img.size)
+			if os.path.exists(save_path):
+				if not os.path.isdir(save_path):
+					raise Exception('保存路径错误！')
+			else:
+				os.mkdir(save_path)
+			root_path = os.path.join(save_path, self.__book_name)
+			if os.path.exists(root_path):
+				print('文件夹：%s 已存在，停止下载《%s》！' % (root_path, self.__book_name))
+				return
+			os.mkdir(root_path)
 
-		print('开始下载《%s》...' % self.__book_name)
-		for key, value in zip(self.__book_items.keys(), self.__book_items.values()):
-			if value[1] <= value[2]:
-				print('正在下载%s...' % key)
-				time.sleep(0.1)
-				for ii in tqdm(range(value[1], value[2] + 1)):
-					pic_name = value[0] % ii
-					canvas.drawImage(self.__book_page_url + pic_name, 0, 0, img.size[0], img.size[1])
-					canvas.showPage()
-				time.sleep(0.1)
-		canvas.save()
-		print('《%s》下载完毕！' % self.__book_name)
+			print('开始下载《%s》...' % self.__book_name)
+			for key, value in zip(self.__book_items.keys(), self.__book_items.values()):
+				if value[1] <= value[2]:
+					print('正在下载%s...' % key)
+					time.sleep(0.1)
+					path = os.path.join(root_path, key)
+					os.mkdir(path)
+					for ii in tqdm(range(value[1], value[2] + 1), unit='页', unit_scale=True, leave=True, miniters=1):
+						pic_name = value[0] % ii
+						urllib.request.urlretrieve(self.__book_page_url + pic_name, os.path.join(path, pic_name))
+					time.sleep(0.1)
+			print('《%s》下载完毕！' % self.__book_name)
+			return True
+		except Exception as ex:
+			print(ex)
+			return False
+		finally:
+			try:
+				if is_download_iso:
+					iso_url_list = self.__get_iso_url(html_page)
+					for ii in range(len(iso_url_list)):
+						self.__download_iso(iso_url_list[ii], save_path, '%s_cd%d.iso' % (self.__book_name, ii + 1))
+			except Exception as ex:
+				print(ex)
+				return False
+
+	def download_pdf(self, book_info_url, save_path, is_download_iso=False):
+		html_page = ''
+		try:
+			# 获取书籍信息页面
+			resp = urllib.request.urlopen(book_info_url)
+			html_page = resp.read().decode('utf-8')
+
+			book_url = self.__get_book_url(html_page)
+			self.__init_para(book_url)
+
+			if os.path.exists(save_path):
+				if not os.path.isdir(save_path):
+					raise Exception('保存路径错误！')
+			else:
+				os.mkdir(save_path)
+			pdf_path = os.path.join(save_path, self.__book_name + '.pdf')
+			if os.path.exists(pdf_path):
+				print('文件：%s 已存在，停止下载《%s》！' % (pdf_path, self.__book_name))
+				return
+			resp = urllib.request.urlopen(
+				self.__book_page_url + self.__book_items['正文页'][0] % self.__book_items['正文页'][1])
+			img = Image.open(resp)
+			canvas = Canvas(pdf_path, pagesize=img.size)
+
+			print('开始下载《%s》...' % self.__book_name)
+			for key, value in zip(self.__book_items.keys(), self.__book_items.values()):
+				if value[1] <= value[2]:
+					print('正在下载%s...' % key)
+					time.sleep(0.1)
+					for ii in tqdm(range(value[1], value[2] + 1), unit='页', leave=True, miniters=1):
+						pic_name = value[0] % ii
+						canvas.drawImage(self.__book_page_url + pic_name, 0, 0, img.size[0], img.size[1])
+						canvas.showPage()
+					time.sleep(0.1)
+			print('正在生成pdf...')
+			canvas.save()
+			print('《%s》下载完毕！' % self.__book_name)
+			return True
+		except Exception as ex:
+			print(ex)
+			return False
+		finally:
+			try:
+				if is_download_iso:
+					iso_url_list = self.__get_iso_url(html_page)
+					for ii in range(len(iso_url_list)):
+						self.__download_iso(iso_url_list[ii], save_path, '%s_cd%d.iso' % (self.__book_name, ii + 1))
+			except Exception as ex:
+				print(ex)
+				return False
 
 	def jpg_to_pdf(self, jpg_root_dir, save_path):
-		if not os.path.isdir(jpg_root_dir):
-			raise Exception('文件夹：%s 不存在！' % jpg_root_dir)
-		self.__book_name = os.path.split(jpg_root_dir)[1]
-		pdf_path = os.path.join(save_path, self.__book_name + '.pdf')
-		if os.path.exists(pdf_path):
-			print('文件：%s 已存在，停止下载《%s》！' % (pdf_path, self.__book_name))
-			return
+		try:
+			if not os.path.isdir(jpg_root_dir):
+				raise Exception('文件夹：%s 不存在！' % jpg_root_dir)
+			self.__book_name = os.path.split(jpg_root_dir)[1]
+			pdf_path = os.path.join(save_path, self.__book_name + '.pdf')
+			if os.path.exists(pdf_path):
+				print('文件：%s 已存在，停止下载《%s》！' % (pdf_path, self.__book_name))
+				return
 
-		value_path = os.path.join(jpg_root_dir, '正文页/' + self.__book_items['正文页'][0] % 1)
-		if not os.path.isfile(value_path):
-			raise Exception('文件夹：%s 无法被识别！' % jpg_root_dir)
-		img = Image.open(value_path)
-		canvas = Canvas(pdf_path, pagesize=img.size)
+			value_path = os.path.join(jpg_root_dir, '正文页/' + self.__book_items['正文页'][0] % 1)
+			if not os.path.isfile(value_path):
+				raise Exception('文件夹：%s 无法被识别！' % jpg_root_dir)
+			img = Image.open(value_path)
+			canvas = Canvas(pdf_path, pagesize=img.size)
 
-		print('开始转换《%s》...' % self.__book_name)
-		for key, value in zip(self.__book_items.keys(), self.__book_items.values()):
-			key_path = os.path.join(jpg_root_dir, key)
-			if os.path.isdir(key_path):
-				print('正在转换%s...' % key)
-				ii = 1
-				while True:
-					value_path = os.path.join(key_path, value[0] % ii)
-					if not os.path.isfile(value_path):
-						self.__set_book_items(key, 1, ii - 1)
-						break
-					canvas.drawImage(value_path, 0, 0, img.size[0], img.size[1])
-					canvas.showPage()
-					ii += 1
-			else:
-				self.__set_book_items(key, 1, 0)
-		canvas.save()
-		print('《%s》转换完毕！' % self.__book_name)
+			print('开始转换《%s》...' % self.__book_name)
+			for key, value in zip(self.__book_items.keys(), self.__book_items.values()):
+				key_path = os.path.join(jpg_root_dir, key)
+				if os.path.isdir(key_path):
+					print('正在转换%s...' % key)
+					ii = 1
+					while True:
+						value_path = os.path.join(key_path, value[0] % ii)
+						if not os.path.isfile(value_path):
+							self.__set_book_items(key, 1, ii - 1)
+							break
+						canvas.drawImage(value_path, 0, 0, img.size[0], img.size[1])
+						canvas.showPage()
+						ii += 1
+				else:
+					self.__set_book_items(key, 1, 0)
+			canvas.save()
+			print('《%s》转换完毕！' % self.__book_name)
+			return True
+		except Exception as ex:
+			print(ex)
+			return False
+
+	def download_resource(self, resource_info_url, save_path):
+		try:
+			# 获取资源信息页面
+			resp = urllib.request.urlopen(resource_info_url)
+			results = re.findall(re.compile(r'<td>(.*?)</td>'), resp.read().decode('utf-8'))
+			if len(results) == 0 and len(results) % 3:
+				raise Exception('获取资源参数失败！')
+
+			for ii in range(0, len(results), 3):
+				url = re.findall(re.compile(r'javascript:SubmitURL\("post","(.*?)"\+escape\("(.*?)"\)\)'),
+				                 results[ii + 2])
+				if len(url) == 1 and len(url[0]) == 2:
+					self.__download_iso(url[0][0] + urllib.parse.quote(url[0][1]), save_path, results[ii])
+			return True
+		except Exception as ex:
+			print(ex)
+			return False
 
 
-if __name__ == '__main__':
+def __main():
 	save_path = os.path.abspath('files')
 	print('默认保存路径：%s' % save_path)
 	user = input('是否修改(y/[n]):')
@@ -303,19 +436,21 @@ if __name__ == '__main__':
 
 	user_mod = input('''请选择下载模式：
 1.根据书籍页面链接下载书籍
-2.根据关键词批量下载所有搜索到的书籍
+2.根据书籍页面链接下载书籍以及电子资源
+3.根据关键词批量下载所有搜索到的书籍
+4.根据关键词批量下载所有搜索到的书籍以及电子资源
 输入相应数字进行选择：''')
 
 	lc = LibraryCrawler()
 	while True:
-		if user_mod == '1':
-			book_url = input('请输入书籍页面链接：')
+		if user_mod == '1' or user_mod == '2':
+			book_url = input('\n请输入书籍页面链接：')
 			try:
-				lc.download_pdf(book_url, save_path)
+				lc.download_pdf(book_url, save_path, True if user_mod == '2' else False)
 			except Exception as ex:
 				print(ex)
-		elif user_mod == '2':
-			key_word = input('请输入搜索关键词：')
+		elif user_mod == '3' or user_mod == '4':
+			key_word = input('\n请输入搜索关键词：')
 			book_list = lc.search_books(key_word)
 			book_num = len(book_list)
 			print('共搜索到%d个结果...' % book_num)
@@ -326,9 +461,13 @@ if __name__ == '__main__':
 			for book_item, ii in zip(book_list, range(book_num)):
 				print('\n准备下载第 %d/%d 本书籍...' % (ii + 1, book_num))
 				try:
-					lc.download_pdf(book_item['url'], save_path)
+					lc.download_pdf(book_item['url'], save_path, True if user_mod == '4' else False)
 				except Exception as ex:
 					print(ex)
 			print('\n所有书籍下载完毕！\n')
 		else:
 			user_mod = input('输入相应数字进行选择(1/2)：')
+
+
+if __name__ == '__main__':
+	__main()
